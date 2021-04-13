b4d5ae505f622c4d6402aee4fe4a6596f8177fed
Anibal Santos <anibalsantosgo@gmail.com>
2021-04-13 20:11:01 +0200

    init

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,25 @@
+/**
+ * Import dependencies from node_modules
+ * see commented examples below
+ */
+
+// import 'some-node-module';
+// import SomeModule from 'some-node-module';
+
+/**
+ * Write any other JavaScript below
+ */
+
+/**
+ * Libraries
+ */
+//* FONT-AWESOME
+
+import "@fortawesome/fontawesome-free/css/all.css";
+
+//* BOOTSTRAP
+
+import * as bootstrap from "bootstrap";
+/**
+ * Libraries
+ */
